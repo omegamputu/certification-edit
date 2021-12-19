765680457a98f85d376c039adf897a827da91350
Omega Mputu <kutekemeni93@gmail.com>
2021-12-19 09:54:06 +0100

    webpack config has undergone a change

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 let mix = require("laravel-mix");
 
-mix.js('src/app.js', 'public/js')
-   .sass('src/app.scss', 'public/css');
+mix.js('src/app.js', 'js').vue()
+.sass('src/sass/app.scss', 'css')
+.sass('src/sass/custom.scss', 'css')
+.setPublicPath('public').browserSync();
