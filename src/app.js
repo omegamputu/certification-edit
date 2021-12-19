a392f30907a22e61dc276e7f2dbf247dd9dc6200
Omega Mputu <kutekemeni93@gmail.com>
2021-12-19 09:51:47 +0100

    Express.js setup, all dev dependencies configured

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-alert('hello world');
+window._ = require('lodash');
+
+window.$ = require('jquery');
+
+window.Popper = require('popper.js');
+
+require('bootstrap');
+
+import Vue from 'vue';
+
+// Components
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
